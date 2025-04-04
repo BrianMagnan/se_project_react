a42ec945de3117157ebcd0e3ec96a3075e570f3b
BrianMagnan <BrianNMagnan@gmail.com>
2025-04-03 21:24:13 -0400

    project 10 cards Styled

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-logo">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img
+          className="header__user-avatar"
+          src={avatar}
+          alt="Terrence Tegegne"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
